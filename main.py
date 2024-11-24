04fbd41007a8af735e3da9dee35ba9e54180f20b
cpasion-office-win10 <373704015@qq.com>
2024-11-24 10:56:49 +0800

    fix: 优化

--- main.py
+++ main.py
@@ -228,3 +228,34 @@ class CpsBeautifySelectRegionCommand(sublime_plugin.TextCommand):
             if int(res["cursorOffset"]) > 0:
                 sublime.Region((res["cursorOffset"], res["cursorOffset"]))
             print("格式化成功:", os.path.basename(self.view.file_name()))
+
+    # 根据语法格式化字符串
+    def beautify_str_by_node(
+        self,
+        buffer_str: str,
+        syntax: str,
+        cursor_offset: int,
+        options: dict,
+        file_path: str = "",
+    ) -> dict:
+        config_str = sublime.encode_value(
+            {
+                "syntax": syntax,  # 当前文件语法
+                "cursorOffset": cursor_offset,  # 当前鼠标位置
+                "filePath": file_path,
+            }
+        )
+
+        options_str = sublime.encode_value(options)
+
+        res = node.run_script(NODE_SCRIPT_PATH, config_str, options_str, buffer_str)
+        if res:
+            try:
+                if isinstance(res, str):
+                    return sublime.decode_value(res)
+
+            except Exception as e:
+                print(3)
+
+                print(Exception("beautify_str_by_node() 执行出错:", e))
+                return False
